b302500881fe03e56fa9068b542cf279ba11e36d
Alex Bresler <abresler@asbcllc.com>
2017-03-25 07:39:20 -0400

    Initial Commit

---
+++ R/entitySearchR-package.R
@@ -0,0 +1,8 @@
+#' API wrappers for legal entity searches
+#'
+#' @name entitySearchR
+#' @source http://pamiec.pl/pa/form/60,Zaloga-SS-KL-Auschwitz.html
+#' @docType package
+#' @author Alex Bresler (abresler@asbcllc.com)
+#' @import curl tidyr rvest dplyr stringr lubridate xml2 purrr stringi readr formattable trelliscopejs
+NULL
